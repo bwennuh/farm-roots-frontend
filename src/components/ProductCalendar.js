f11a78fbf336b676c9c820214de6d7062d0ca85c
bwennuh <brennabruening@gmail.com>
2021-06-21 15:31:56 -0400

    Add start date and end date state hooks

--- src/components/ProductCalendar.js
+++ src/components/ProductCalendar.js
@@ -3,7 +3,7 @@ import {useState} from 'react'
 import { DateRange } from 'react-date-range';
 
 
-export default function ProductCalendar() {
+export default function ProductCalendar(props) {
     const [state, setState] = useState([
         {
         startDate: new Date(),
@@ -12,10 +12,29 @@ export default function ProductCalendar() {
         }
     ]);
 
+    const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+
     function getDates(){
         
-        console.log(Date.parse(state[0].startDate))
+        // console.log(Date.parse(state[0].startDate))
+        // console.log(state[0].endDate)
+
+        console.log("Start date examples:")
+        console.log(state[0].startDate)
+        // console.log(state[0].startDate.toDateString())
+        // console.log(state[0].startDate.toLocaleDateString())
+        console.log(state[0].startDate.toLocaleDateString('us-US', options))
+
+        console.log("---------")
+        console.log("End date examples:")
         console.log(state[0].endDate)
+        // console.log(state[0].endDate.toLocaleDateString())
+
+        let startDate = state[0].startDate
+        let endDate = state[0].endDate
+
+        props.showDateSelections(startDate, endDate)
+        // console.log(props)
     };
     
 
@@ -28,6 +47,7 @@ export default function ProductCalendar() {
             moveRangeOnFirstSelection={false}
             ranges={state}
             />
+            
         </div>
     )
 }
