cff9dc5723f1261ad49b752fa86b905fa7e33829
Alex Kravecas <akravecas@yahoo.com>
2021-06-23 15:49:02 -0400

    basically nothing

--- src/components/FarmCardContainer.js
+++ src/components/FarmCardContainer.js
@@ -5,19 +5,38 @@ import FarmCard from './FarmCard.js'
 
 class FarmCardContainer extends Component {
 
+  state = {
+    produce: []
+  }
+
+
+  sendProduceToCards = (id) => {
+      let produce = this.props.products.filter(product => product.farm_id === id)
+      console.log(produce)
+      // this.setState({
+      //   produce: produce
+      // })
+  }
 
 
   render(){
 
-    let farmArray = ["Farm 1", "Farm 2", "Farm 3"]
-    let productsArray = ["banana", "apple", "carrot", "corn"]
+    let farmArray = this.props.farms
+
+
+    // console.log(farmArray)
+    // let productsArray = this.props.products
 
     return(
       <div>
 
         <h1>Farm Card Container</h1>
-        <FarmCard farm="Brenna" addToCart={this.props.addToCart} products={productsArray}/>
-        {farmArray.map(farm => <FarmCard farm={farm} addToCart={this.props.addToCart} products={productsArray} />)}
+
+        {farmArray.map(farm => this.sendProduceToCards(farm.id))}
+        
+        {farmArray.map(farm => (<FarmCard farm={farm.name} addToCart={this.props.addToCart} produce = {this.state.produce}/>
+         ))}
+      
 
       </div>
     )
